850033e8de6527200e5851e0b4df4e959bcd2ab4
stickler-ci <support@stickler-ci.com>
2020-05-06 07:48:46 +0000

    Fixing style errors.

--- function.py
+++ function.py
@@ -5,5 +5,6 @@ Created on Wed May  6 09:32:28 2020
 @author: zwickl-nb
 """
 
-def add(a,b):
+
+def add(a, b):
     return a + b
--- test_function.py
+++ test_function.py
@@ -5,6 +5,6 @@ Created on Wed May  6 09:33:25 2020
 @author: zwickl-nb
 """
 
+
 def test_add():
-    assert(2,1) == 3
-    
+    assert(2, 1) == 3
