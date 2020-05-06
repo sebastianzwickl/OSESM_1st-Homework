52bc3b7fd2e94bf33df3bb90092d22d43cfdb788
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2020-05-06 09:34:29 +0200

    Add function and test
    adding the add function and test of the function

---
+++ function.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 09:32:28 2020
+
+@author: zwickl-nb
+"""
+
+def function add(a,b):
+    return a + b
---
+++ test_function.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May  6 09:33:25 2020
+
+@author: zwickl-nb
+"""
+
+def test_add():
+    assert(2,1) == 3
+    
