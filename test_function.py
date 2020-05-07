b465af945f844ab9186fe50e93d2b54114c98a3b
stickler-ci <support@stickler-ci.com>
2020-05-07 09:24:11 +0000

    Fixing style errors.

--- test_function.py
+++ test_function.py
@@ -8,16 +8,14 @@ from function import add
 from function import validate
 import pandas as pd
 
+
 def test_add():
     assert add(2, 1) == 3
 
 
 def test_validate():
-    cap = {'c':50}
+    cap = {'c': 50}
     cap = pd.DataFrame(cap, index=[0])
-    demand = {'d':20}
+    demand = {'d': 20}
     demand = pd.DataFrame(demand, index=[0])
     assert validate(cap, demand) == 1
-    
-
-
