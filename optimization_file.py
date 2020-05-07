de60f0c809261590e9e19c37397b9e7890fbaeb1
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2020-05-07 10:00:35 +0200

    Update LP

--- optimization_file.py
+++ optimization_file.py
@@ -6,15 +6,15 @@ Created on Wed May  6 15:00:18 2020
 """
 
 import pandas as pd
-from function import optimize
-import numpy as np
-from pyomo.environ import *
+import pyomo.environ as py
+
 
 def cost_rule(m):
      costs = 0
      for i in indexlist:
         costs += model.x[i] * m.frame[i][0]
      return costs
+
 
 def demand_rule(m):
     supply = 0
@@ -23,45 +23,34 @@ def demand_rule(m):
     return supply >= m.demand[0]
 
 
-    
-
 mc = pd.read_excel('data.xlsx','marginal costs', index=False)
 mc.reset_index(drop=True)
-
 demand = pd.read_excel('data.xlsx', 'demand')
 inter = pd.read_excel('data.xlsx', 'intermittent')
 
 
-
-
-
-model = ConcreteModel()
+model = py.ConcreteModel()
 model.frame = mc
 model.demand = demand.values[0]
 model.inter = inter
-
 
 mc_dict = mc.to_dict()
 indexlist = set()
     
 for key in mc_dict:
-#    print(key)
-#    print(mc[key])
     indexlist.add(key)
 
-#for i in indexlist:
-#    print(mc[i][0])
     
-model.il = Set(initialize=indexlist) 
+model.il = py.Set(initialize=indexlist) 
 #model.d = demand.values
-model.x = Var(indexlist, within = NonNegativeReals)
-model.c1 = Constraint(rule = demand_rule,
+model.x = py.Var(indexlist, within = py.NonNegativeReals)
+model.c1 = py.Constraint(rule = demand_rule,
                       doc='Supply equals demand')
 
-model.obj = Objective(rule = cost_rule,
-                      sense = minimize,
+model.obj = py.Objective(rule = cost_rule,
+                      sense = py.minimize,
                       doc='minimize costs of supply')
-opt = SolverFactory('glpk')
+opt = py.SolverFactory('glpk')
 opt.solve(model)
 for i in indexlist:
     print(i+':',model.x[i].value)
