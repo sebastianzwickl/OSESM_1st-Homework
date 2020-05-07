130e2cbfa972e300e124eed19ed2b9abf03361d1
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2020-05-07 10:50:09 +0200

    Update the test script
    test also the validation function

--- test_function.py
+++ test_function.py
@@ -5,7 +5,13 @@ Created on Wed May  6 09:33:25 2020
 @author: zwickl-nb
 """
 from function import add
+from function import validate
+import pandas as pd
 
 
 def test_add():
     assert add(2, 1) == 3
+
+
+def test_validate():
+    assert validate(50, 20) == True
