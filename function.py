ea4b5ab97d0d096f730d5374bdbd051b1ae27961
Sebastian Zwickl-Bernhard <zwickl@eeg.tuwien.ac.at>
2020-05-07 10:37:56 +0200

    Update python script

--- function.py
+++ function.py
@@ -4,40 +4,22 @@ Created on Wed May  6 09:32:28 2020
 
 @author: zwickl-nb
 """
-from pyomo.environ import *
-import numpy as np
 
 
 def add(a, b):
     return a + b
 
-#def validation(a,b,c):
-#    if bool((bool(a) + bool(b) + bool(c))):
-#        return True
-#    else:
-#        return False
 
-#def optimize(mc, demand):
-#    model = ConcreteModel()
-#    mc_dict = mc.to_dict()
-#    indexlist = set()
-#    
-#    for key in mc_dict:
-#        indexlist.add(key)
-#
-#    model.d = demand.values
-#    
-#    
-#
-#    model.c = Param(indexlist, initialize = mc_dict)
-#    print(model.c.__call__)
-#
-#    model.x = Var(indexlist, within = NonNegativeReals)
+def validate(cap, demand):
+    capacities = sum(sum(cap.values))
+    if capacities >= sum(demand.values):
+        return True
+    else:
+        return False
 
-#    model.c1 = Constraint(expr = model.x[1] >= model.d)
-#    model.obj = Objective(
-#            expr = model.x[1] ,sense = minimize)
-#    opt = SolverFactory('glpk')
-#    opt.solve(model)
-#    print(model.x[1].value)
-#    return    
+
+
+    
+
+
+
--- optimization_file.py
+++ optimization_file.py
@@ -7,6 +7,7 @@ Created on Wed May  6 15:00:18 2020
 
 import pandas as pd
 import pyomo.environ as py
+from function import validate
 
 
 def cost_rule(m):
@@ -23,36 +24,39 @@ def demand_rule(m):
     return supply >= m.demand[0]
 
 
+def cap_rule(m, i):
+    return m.x[i] <= m.inter[i][0]
+    
+
+
 mc = pd.read_excel('data.xlsx','marginal costs', index=False)
 mc.reset_index(drop=True)
 demand = pd.read_excel('data.xlsx', 'demand')
 inter = pd.read_excel('data.xlsx', 'intermittent')
 
+if validate(inter, demand):
+    model = py.ConcreteModel()
+    model.frame = mc
+    model.demand = demand.values[0]
+    model.inter = inter
 
-model = py.ConcreteModel()
-model.frame = mc
-model.demand = demand.values[0]
-model.inter = inter
-
-mc_dict = mc.to_dict()
-indexlist = set()
+    mc_dict = mc.to_dict()
+    indexlist = set()
     
-for key in mc_dict:
-    indexlist.add(key)
+    for key in mc_dict:
+        indexlist.add(key)
 
-    
-model.il = py.Set(initialize=indexlist) 
-#model.d = demand.values
-model.x = py.Var(indexlist, within = py.NonNegativeReals)
-model.c1 = py.Constraint(rule = demand_rule,
-                      doc='Supply equals demand')
+    model.il = indexlist 
+    model.x = py.Var(indexlist, within = py.NonNegativeReals)
+    model.c1 = py.Constraint(rule = demand_rule,
+                             doc='Supply equals demand')
+    model.c2 = py.Constraint(model.il, rule = cap_rule,
+                             doc='intermittent supply and capacities')
+    model.obj = py.Objective(rule = cost_rule,
+                             sense = py.minimize,
+                             doc='minimize costs of supply')
+    opt = py.SolverFactory('glpk')
+    opt.solve(model)
+    for i in indexlist:
+        print(i,':',model.x[i].value)
 
-model.obj = py.Objective(rule = cost_rule,
-                      sense = py.minimize,
-                      doc='minimize costs of supply')
-opt = py.SolverFactory('glpk')
-opt.solve(model)
-for i in indexlist:
-    print(i+':',model.x[i].value)
-
-#optimize(mc, demand)
