fef928cd4965e3226e098b1fde445dcc909bf127
stickler-ci <support@stickler-ci.com>
2020-05-07 08:09:48 +0000

    Fixing style errors.

--- optimization_file.py
+++ optimization_file.py
@@ -10,10 +10,10 @@ import pyomo.environ as py
 
 
 def cost_rule(m):
-     costs = 0
-     for i in indexlist:
+    costs = 0
+    for i in indexlist:
         costs += model.x[i] * m.frame[i][0]
-     return costs
+    return costs
 
 
 def demand_rule(m):
@@ -23,7 +23,7 @@ def demand_rule(m):
     return supply >= m.demand[0]
 
 
-mc = pd.read_excel('data.xlsx','marginal costs', index=False)
+mc = pd.read_excel('data.xlsx', 'marginal costs', index=False)
 mc.reset_index(drop=True)
 demand = pd.read_excel('data.xlsx', 'demand')
 inter = pd.read_excel('data.xlsx', 'intermittent')
@@ -36,23 +36,23 @@ model.inter = inter
 
 mc_dict = mc.to_dict()
 indexlist = set()
-    
+
 for key in mc_dict:
     indexlist.add(key)
 
-    
-model.il = py.Set(initialize=indexlist) 
-#model.d = demand.values
-model.x = py.Var(indexlist, within = py.NonNegativeReals)
-model.c1 = py.Constraint(rule = demand_rule,
-                      doc='Supply equals demand')
 
-model.obj = py.Objective(rule = cost_rule,
-                      sense = py.minimize,
-                      doc='minimize costs of supply')
+model.il = py.Set(initialize=indexlist)
+#model.d = demand.values
+model.x = py.Var(indexlist, within=py.NonNegativeReals)
+model.c1 = py.Constraint(rule=demand_rule,
+                         doc='Supply equals demand')
+
+model.obj = py.Objective(rule=cost_rule,
+                         sense=py.minimize,
+                         doc='minimize costs of supply')
 opt = py.SolverFactory('glpk')
 opt.solve(model)
 for i in indexlist:
-    print(i+':',model.x[i].value)
+    print(i+':', model.x[i].value)
 
 #optimize(mc, demand)
